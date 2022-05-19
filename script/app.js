fe0e3e92a31f1156f328fbd335598a68487737ef
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-05-18 23:02:22 -0500

    add new route cars

--- script/app.js
+++ script/app.js
@@ -1,45 +1,63 @@
 const $formTitanic = document.getElementById("titanicForm");
+const $formCars = document.getElementById("carsForm");
 const server = "http://localhost:3000/titanic";
 
 const renderDataSurvived = (data) => {
-  const $rowResult = document.getElementById("tr-results");
+  const $rowResult = document.getElementById("tr-titanic");
   const $imgState = document.getElementById("imageState");
 
   $rowResult.innerHTML = "";
 
-  $rowResult.innerHTML = `
-    <td>${data.survived}</td>
-    <td>${data.pclass}</td>
-    <td>${data.sex}</td>
-    <td>${data.age}</td>
-    <td>${data.sibsp}</td>
-    <td>${data.parch}</td>
-    <td>${data.fare}</td>
-    <td>${data.embarked}</td>
-    <td>${data.scoreLabel}</td>
-    <td>${data.scoreProbability}</td>
-  `;
+  for (let key in data) {
+    console.log(data[key]);
+    $rowResult.innerHTML += `<td>${data[key]}</td>`;
+  }
 
   data.scoreProbability > 0.5
     ? ($imgState.src = "../images/survived.png")
     : ($imgState.src = "../images/dead.png");
 };
 
-$formTitanic.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const formData = new FormData($formTitanic);
-  const data = Object.fromEntries(formData);
+if (document.getElementById("titanicForm") != null) {
+  $formTitanic.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData($formTitanic);
+    const data = Object.fromEntries(formData);
 
-  // Fetching the data of the form with the method POST
-  fetch(server, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      renderDataSurvived(data);
-    });
-});
+    // Fetching the data of the form with the method POST
+    fetch("http://localhost:3000/titanic", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        renderDataSurvived(data);
+      });
+  });
+}
+
+if (document.getElementById("carsForm") != null) {
+  $formCars.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.preventDefault();
+    const formData = new FormData($formCars);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    fetch("http://localhost:3000/cars", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+}
