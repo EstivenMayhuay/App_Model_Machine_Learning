c5450471f7e357e540f16facc017786a7633f5a6
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-05-25 18:32:28 -0500

    add a model by represent the data, create a select and options by the fields and Added a links for my social media

--- script/app.js
+++ script/app.js
@@ -1,90 +1,23 @@
-const $formTitanic = document.getElementById("titanicForm");
-const $formCars = document.getElementById("carsForm");
-const $btnCloseModal = document.getElementById("btnCloseModal");
+import { formCars, formTitanic } from "./modules/formModel.js";
 
-const renderDataSurvived = (data, elem, label) => {
-  const $rowResult = document.querySelector(elem);
-  const $imgState = document.getElementById("imageState");
-  const $sectionResults =
-    document.querySelector(elem).parentElement.parentElement.parentElement;
+const d = document;
 
-  $rowResult.innerHTML = "";
+d.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  for (let key in data) {
-    if (key == "scoreLabel") {
-      data[key] = Number(data[key]).toFixed(2);
-    }
-    $rowResult.innerHTML += `<td>${data[key]}</td>`;
-  }
+  if (e.target.id === "titanicForm") formTitanic(e.target);
+  else if (e.target.id === "carsForm") formCars(e.target);
+});
 
-  if (label == "titanic") {
-    data.scoreProbability > 0.5
-      ? ($imgState.src = "../images/survived.png")
-      : ($imgState.src = "../images/dead.png");
-  } else if (label == "cars") {
-    Number(data.scoreLabel) > Number(data.price)
-      ? ($imgState.src = "../images/increase.png")
-      : ($imgState.src = "../images/decrease.png");
-  }
-
-  // Open the modal
-  $sectionResults.style.transform = "translate(0, 0)";
-  $sectionResults.style.transition = "transform 0.4s ease";
-};
-
-if (document.getElementById("titanicForm") != null) {
-  $formTitanic.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData($formTitanic);
-    const data = Object.fromEntries(formData);
-
-    console.log(data);
-
-    // Fetching the data of the form with the method POST
-    fetch("https://emcodyapiml.herokuapp.com/titanic", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        renderDataSurvived(data, "#tr-titanic", "titanic");
-      });
-  });
-}
-
-if (document.getElementById("carsForm") != null) {
-  $formCars.addEventListener("submit", (e) => {
-    e.preventDefault();
-    e.preventDefault();
-    const formData = new FormData($formCars);
-    const data = Object.fromEntries(formData);
-
-    console.log(data);
-
-    // Fetching the data of the form with the method POST
-    fetch("https://emcodyapiml.herokuapp.com/cars", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        renderDataSurvived(data, "#tr-cars", "cars");
-      });
-  });
-}
-
-if (document.getElementById("btnCloseModal") != null) {
-  $btnCloseModal.addEventListener("click", (e) => {
-    const $modal = e.target.parentElement;
+d.addEventListener("click", (e) => {
+  if (
+    e.target.matches("#btnCloseModal") ||
+    e.target.matches("#btnCloseModal *")
+  ) {
+    const $modal = e.target.parentElement.parentElement;
 
     // Close the modal
-    $modal.style.transform = "translate(0, -100%)";
-    $modal.style.transition = "transform 0.4s ease";
-  });
-}
+    $modal.style.transform = "translate(-100%, 0)";
+    $modal.style.transition = "transform 0.8s ease";
+  }
+});
---
+++ script/modules/formModel.js
@@ -0,0 +1,77 @@
+const renderDataSurvived = (data, elem, label) => {
+  const $rowResult = document.querySelector(elem);
+  const $imgState = document.getElementById("imageState");
+  const $sectionResults =
+    document.querySelector(elem).parentElement.parentElement.parentElement
+      .parentElement;
+  const $paragraphResult = document.querySelector("#paragraphResult");
+
+  $rowResult.innerHTML = "";
+
+  for (let key in data) {
+    if (key == "scoreLabel") {
+      data[key] = Number(data[key]).toFixed(2);
+    }
+    $rowResult.innerHTML += `<td>${data[key]}</td>`;
+  }
+
+  if (label == "titanic") {
+    if (data.scoreProbability > 0.5) {
+      $imgState.src = "../images/survived.png";
+      $paragraphResult.innerHTML = `Hey 😀, we're happy for you. You will survive the crash of the titanic. You have a <strong>${data.scoreProbability}</strong> chance of surviving`;
+    } else {
+      $imgState.src = "../images/dead.png";
+      $paragraphResult.textContent = `Hey 😔, we're sad for you. You will not survive the titanic crash. You have a ${data.scoreProbability} chance of surviving`;
+    }
+  } else if (label == "cars") {
+    if (Number(data.scoreLabel) > Number(data.price)) {
+      $imgState.src = "../images/increase.png";
+      $paragraphResult.innerHTML = `The real price of your car ${data.make} with a ${data.horsepower} horsepower and with the following price ${data.price} <strong>should be ${data.scoreLabel}</strong>. In short, <strong>the car has a higher price</strong>`;
+    } else {
+      $imgState.src = "../images/decrease.png";
+      $paragraphResult.innerHTML = `The real price of your car ${data.make} with a ${data.horsepower} horsepower and the following price ${data.price} <strong>should be ${data.scoreLabel}</strong>. In short, <strong>the car has a lower price.</strong>`;
+    }
+  }
+
+  // Open the modal
+  $sectionResults.style.transform = "translate(0, 0)";
+  $sectionResults.style.transition = "transform 0.8s ease";
+};
+
+const formTitanic = (elem) => {
+  const formData = new FormData(elem);
+  const data = Object.fromEntries(formData);
+
+  // Fetching the data of the form with the method POST
+  fetch("https://emcodyapiml.herokuapp.com/titanic", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      renderDataSurvived(data, "#tr-titanic", "titanic");
+    });
+};
+
+const formCars = (elem) => {
+  const formData = new FormData(elem);
+  const data = Object.fromEntries(formData);
+
+  // Fetching the data of the form with the method POST
+  fetch("https://emcodyapiml.herokuapp.com/cars", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      renderDataSurvived(data, "#tr-cars", "cars");
+    });
+};
+
+export { formTitanic, formCars };
